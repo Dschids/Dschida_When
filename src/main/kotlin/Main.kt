4b5f2566e671e973d81080c7404fde7c82dc11b4
Bob Dschida <rdschida@students.mchenry.edu>
2023-08-24 16:01:16 -0500

    comments

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,12 +1,14 @@
 fun main(args: Array<String>) {
 // asks user for a number between 1 and 10, then tells the user what that number is in German
-
+// intialize variables
 var usernum = 1
 var inGerman = "x"
 var cont = "y"
+    // run while loop until user enters n to stop
 while (cont != "n") {
     print("Enter a number for the German translation: ")
     usernum = readln()!!.toInt()
+    // when statement changes ingerman variable depending on number entered
     when (usernum){
         1 -> inGerman = "Einz"
         2 -> inGerman = "Zwei"
@@ -19,8 +21,8 @@ while (cont != "n") {
         9 -> inGerman = "Nuen"
         10 -> inGerman = "Zehn"
     }
-    println("$usernum in German is $inGerman")
+    println("$usernum in German is $inGerman") // print number entered and german version
     print("Would you like to enter another number? y/n: ")
-    cont = readln()!!.toString()
+    cont = readln()!!.toString() //update loop flag with user response
 }
 }
